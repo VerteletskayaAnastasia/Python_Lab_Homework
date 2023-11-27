12901e88a9033aa37d02425ce3f1853e5b0e9f8a
VerteletskayaAnastasia <149201007+VerteletskayaAnastasia@users.noreply.github.com>
2023-11-27 20:05:40 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,8 +2,7 @@
 import json
 def task() -> float:
     with open('input.json', "r", encoding='utf-8') as f:
-        ans=0
-        for _ in json.load(f):
-            ans= ans+float(_['score']*_['weight'])
-        return (round(ans,3))
+        ans=[_['score']*_['weight'] for _ in json.load(f)]
+        ans=sum(ans)
+    return round(ans,3)
 print(task())
