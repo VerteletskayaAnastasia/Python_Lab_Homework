75b90fbaf6709f9ffcb18412f8deef8334f8d72f
VerteletskayaAnastasia <149201007+VerteletskayaAnastasia@users.noreply.github.com>
2024-02-13 01:31:11 +0300

    Update лаб3.py

--- лаб3.py
+++ лаб3.py
@@ -9,6 +9,14 @@ class Book:
         self._name = name
         self._author = author
 
+    @property
+    def name(self)-> str:
+        return self._name
+
+    @property
+    def author(self)-> str:
+        return self._author
+
     def __str__(self):
         """ Метод str, возвращающий имя и автора книги"""
         return f"Книга {self._name}. Автор {self._author}"
@@ -27,11 +35,18 @@ class PaperBook(Book):
 
                          """
         super().__init__(name, author)
-        if not isinstance(pages, int):
-            raise TypeError("Количество страниц должно быть типа int")
-        if pages <= 0:
-            raise ValueError("Количество страниц должно быть положительным числом")
         self.pages = pages
+    @property
+    def pages(self) -> str:
+        return self._pages
+
+    @pages.setter
+    def pages(self, new_pages: str) -> str:
+        if not isinstance(new_pages, int):
+            raise TypeError("Количество страниц должно быть типа int")
+        if new_pages <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
+        self._pages = new_pages
 
     def __repr__(self):
         """ Метод repr, возвращающий имя,автора книги, число страниц"""
@@ -47,11 +62,19 @@ class AudioBook(Book):
 
                                  """
         super().__init__(name, author)
-        if not isinstance(duration, (int, float)):
-            raise TypeError("Продолжительность должна быть типа int или float")
-        if duration <= 0:
-            raise ValueError("Продолжительность должна быть положительным числом")
         self.duration = duration
+
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self, new_duration: float) -> float:
+        if not isinstance(new_duration, (int, float)):
+            raise TypeError("Продолжительность должна быть типа int или float")
+        if new_duration <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом")
+        self._duration = new_duration
 
     def __repr__(self):
         """ Метод repr, возвращающий имя,автора книги, продолжительность"""
